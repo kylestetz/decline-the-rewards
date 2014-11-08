6fedaad061266a1b89832772f079268d5dbd0a4c
Jimmy Harrison <jtharrison91@gmail.com>
2014-11-08 11:59:52 -0500

    changes to game.js config

--- public/js/config/game.js
+++ public/js/config/game.js
@@ -7,17 +7,41 @@ module.exports = {
       items: [
         {
           name: "car",
+          prompt: "wanna car?",
           image: "car.svg"
         },
         {
           name: "insurance",
+          prompt: "want car insurance?",
           incessant: true,
           image: "insurance.svg"
         },
         {
           name: "oil change",
+          prompt: "oil change?",
           incessant: true,
           image: "oil-change.svg"
+        }
+      ]
+    },
+    {
+      pathName: "house",
+      items: [
+        {
+          name: "house",
+          prompt: "wanna house?",
+          image: "house.svg"
+        },
+        {
+          name: "add porch",
+          prompt: "add a porch?",
+          image: "porch.svg"
+        },
+        {
+          name: "mortgage",
+          prompt: "pay the mortgage",
+          incessant: true,
+          image: "mortgage.svg"
         }
       ]
     }
