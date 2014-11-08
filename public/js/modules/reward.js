cfc7d4f89ce64e7421a82b253eb1b15beea69400
Kyle Stetz <kyle@punkave.com>
2014-11-08 11:15:24 -0500

    rewards and assets

--- public/js/modules/reward.js
+++ public/js/modules/reward.js
@@ -1,4 +1,15 @@
 
 module.exports = function(config) {
   var self = this;
+
+  var $object = $('#' + config.name);
+
+  self.show = function() {
+    // plop it in game land
+    $('.game').append($object);
+    // make it draggable, contain it to the window
+    $object.draggable({
+      containment: $('.game')
+    });
+  };
 };
