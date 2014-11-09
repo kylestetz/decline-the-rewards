de54ec993f5ef397b5cfac7a78544e1cf11949b0
Jimmy Harrison <jtharrison91@gmail.com>
2014-11-09 18:40:39 -0500

    refactored declineTheApi some, made incessant items incessant

--- public/js/modules/declineTheApi.js
+++ public/js/modules/declineTheApi.js
@@ -4,57 +4,62 @@
   var api = {};
 
   var decisions = {};
+  var prompted = {};
+  var incessantMin = 5000;
+  var incessantMax = 25000;
 
-  api.choose = function (accept, mainIndex, sideIndex) {
+  api.choose = function (accept, mainIndex, sideIndex, incessant) {
     if (accept) {
-      if (sideIndex == 0 && typeof config.paths[mainIndex + 1] != 'undefined') {
-        addMainDecision(mainIndex + 1);
+      if (sideIndex == 0 && !isUndefined(config.paths[mainIndex + 1])) {
+        addDecision(mainIndex + 1, 0);
       }
-      if (typeof config.paths[mainIndex].items[sideIndex + 1] != 'undefined') {
-        addSideDecision(mainIndex, sideIndex + 1);
+      if (!isUndefined(config.paths[mainIndex].items[sideIndex + 1])) {
+        addDecision(mainIndex, sideIndex + 1);
       }
+      if (!isUndefined(decisions[mainIndex][sideIndex]) && (config.paths[mainIndex].items[sideIndex].incessant)) {
+        decisions[mainIndex][sideIndex].callback = function() {setTimeout(decisions[mainIndex][sideIndex].showPrompt, getRandomInt(incessantMin,incessantMax))};
+        setTimeout(decisions[mainIndex][sideIndex].showPrompt, getRandomInt(incessantMin,incessantMax));
+      }
+
     } else {
       // maybe some alternative logic for decline
     }
   }
 
-  function addMainDecision(mainIndex) {
-    decisions[mainIndex] = [];
-    var item = config.paths[mainIndex].items[0];
+  function addDecision(mainIndex, sideIndex) {
+    if (isUndefined(decisions[mainIndex])) {
+      decisions[mainIndex] = [];
+    }
+    var cfgItem = config.paths[mainIndex].items[sideIndex];
 
-    var decision = new Decision({
-      prompt: item.prompt,
-      image: item.image,
-      callback: function(accept) {
-        api.choose(accept, mainIndex, 0);
-      }
-    });
-    decisions[mainIndex].push(decision);
-
-    decision.showPrompt();
-    console.log(decision.promptText);
-  }
-
-  function addSideDecision(mainIndex, sideIndex) {
-    var item = config.paths[mainIndex].items[sideIndex];
-
-    var decision = new Decision({
-      prompt: item.prompt,
-      image: item.image,
-      position: decisions[mainIndex][0].getPosition(),
+    var decisionOpts = {
+      prompt: cfgItem.prompt,
+      image: cfgItem.image,
       callback: function(accept) {
         api.choose(accept, mainIndex, sideIndex);
       }
-    });
+    }
 
+    if (sideIndex != 0) {
+      decisionOpts.position = decisions[mainIndex][0].getPosition();
+    }
+
+    var decision = new Decision(decisionOpts); 
     decisions[mainIndex].push(decision);
 
-    setTimeout(decision.showPrompt, getRandomInt(1000,10000));
-    console.log(decision.promptText);
+    decision.showPrompt();
   }
 
   // start first decision
-  addMainDecision(0);
+  addDecision(0, 0);
+
+  function isUndefined(arg) {
+    if (typeof arg == 'undefined') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
